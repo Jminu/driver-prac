49295870c2a97fad4ddff3f847e961811d4e1742
Jin Minu <s9430939@naver.com>
2025-07-21 22:52:45 +0900

    kernel build

---
+++ arm-gnu-toolchain-14.3.rel1-aarch64-aarch64-none-linux-gnu/lib/gcc/aarch64-none-linux-gnu/14.3.1/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/src/gcc/configure --target=aarch64-none-linux-gnu --prefix= --with-sysroot=/aarch64-none-linux-gnu/libc --with-build-sysroot=/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/build-aarch64-none-linux-gnu/install//aarch64-none-linux-gnu/libc --with-bugurl=https://bugs.linaro.org/ --enable-gnu-indirect-function --enable-shared --disable-libssp --disable-libmudflap --enable-checking=release --enable-languages=c,c++,fortran --with-gmp=/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/build-aarch64-none-linux-gnu/host-tools --with-mpfr=/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/build-aarch64-none-linux-gnu/host-tools --with-mpc=/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/build-aarch64-none-linux-gnu/host-tools --with-isl=/tmp/jenkins-f07d89a2/workspace/GNU-toolchain/arm-14/build-aarch64-none-linux-gnu/host-tools --enable-fix-cortex-a53-843419 --with-pkgversion='Arm GNU Toolchain 14.3.Rel1 (Build arm-14.174)'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
